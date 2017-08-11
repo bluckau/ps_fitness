c104cc2dfbd84e3ca4cec5a7038dbbb529b3543e
Brian Luckau <bluckau@luckau.us>
2017-08-11 10:15:38 -0600

    checking in annotated version of the basic app

--- spring_sample_xml_anno/src/main/java/Application.java
+++ spring_sample_xml_anno/src/main/java/Application.java
@@ -1,12 +1,15 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import com.pluralsight.service.CustomerService;
-import com.pluralsight.service.CustomerServiceImpl;
 
-public class Application {
+public class Application
+{
 
-	public static void main(String[] args) {
-		CustomerService service = new CustomerServiceImpl();
-		
+	public static void main(String[] args)
+	{
+		ApplicationContext appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+		CustomerService service = appContext.getBean("customerService", CustomerService.class);
 		System.out.println(service.findAll().get(0).getFirstName());
-	
 	}
 }
--- spring_sample_xml_anno/src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ spring_sample_xml_anno/src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -2,20 +2,24 @@ package com.pluralsight.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.pluralsight.model.Customer;
 import com.pluralsight.repository.CustomerRepository;
-import com.pluralsight.repository.HibernateCustomerRepositoryImpl;
 
-public class CustomerServiceImpl implements CustomerService {
-	
-	private CustomerRepository customerRepository = new HibernateCustomerRepositoryImpl();
-	
-	/* (non-Javadoc)
-	 * @see com.pluralsight.service.CustomerService#findAll()
-	 */
+
+@Service("customerService")
+public class CustomerServiceImpl implements CustomerService
+{
+	@Autowired
+	private CustomerRepository customerRepository;
+
+
 	@Override
-	public List<Customer> findAll() {
+	public List<Customer> findAll()
+	{
 		return customerRepository.findAll();
 	}
-	
+
 }
