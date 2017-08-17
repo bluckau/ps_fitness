d8a17484bf226fefe8dae79746fef49a53058057
Brian Luckau <bluckau@luckau.us>
2017-08-17 14:33:33 -0600

    using scopes

--- spring_sample/src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ spring_sample/src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -6,16 +6,20 @@ import com.pluralsight.model.Customer;
 import com.pluralsight.repository.CustomerRepository;
 import com.pluralsight.repository.HibernateCustomerRepositoryImpl;
 
-public class CustomerServiceImpl implements CustomerService {
-	
+public class CustomerServiceImpl implements CustomerService
+{
+
 	private CustomerRepository customerRepository = new HibernateCustomerRepositoryImpl();
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.pluralsight.service.CustomerService#findAll()
 	 */
 	@Override
-	public List<Customer> findAll() {
+	public List<Customer> findAll()
+	{
 		return customerRepository.findAll();
 	}
-	
+
 }
