3986a6618973705b7b2ba3d4110c1fe18809ccba
Brian Luckau <bluckau@luckau.us>
2017-08-17 14:00:34 -0600

    using constructor injection

--- spring_sample_java/src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ spring_sample_java/src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -10,6 +10,11 @@ public class CustomerServiceImpl implements CustomerService
 
 	private CustomerRepository customerRepository;
 
+	public CustomerServiceImpl(CustomerRepository customerRepository)
+	{
+		this.customerRepository = customerRepository;
+	}
+
 	public void setCustomerRepository(CustomerRepository customerRepository)
 	{
 		this.customerRepository = customerRepository;
