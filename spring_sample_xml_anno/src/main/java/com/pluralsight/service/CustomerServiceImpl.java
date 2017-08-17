becb837e4ebbc57f7ec0db8b05700b3961704c58
Brian Luckau <bluckau@luckau.us>
2017-08-16 19:35:17 -0600

    committing the anonotation flavor

--- spring_sample_xml_anno/src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ spring_sample_xml_anno/src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -8,13 +8,25 @@ import org.springframework.stereotype.Service;
 import com.pluralsight.model.Customer;
 import com.pluralsight.repository.CustomerRepository;
 
-
 @Service("customerService")
 public class CustomerServiceImpl implements CustomerService
 {
-	@Autowired
+	// @Autowired
 	private CustomerRepository customerRepository;
 
+	@Autowired
+	public CustomerServiceImpl(CustomerRepository customerRepository)
+	{
+		System.out.println("We are using constructor injection");
+		this.customerRepository = customerRepository;
+	}
+
+	// @Autowired
+	public void setCustomerRepository(CustomerRepository customerRepository)
+	{
+		System.out.println("We are using setter injection");
+		this.customerRepository = customerRepository;
+	}
 
 	@Override
 	public List<Customer> findAll()
