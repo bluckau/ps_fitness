b45169dd0403525418a3d8fcf6c4852deafbfc3f
Brian Luckau <bluckau@luckau.us>
2017-08-18 09:58:44 -0600

    added properties to the xml flavor

--- spring_sample_xml/src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
+++ spring_sample_xml/src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
@@ -3,17 +3,25 @@ package com.pluralsight.repository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import com.pluralsight.model.Customer;
 
-public class HibernateCustomerRepositoryImpl implements CustomerRepository {
+public class HibernateCustomerRepositoryImpl implements CustomerRepository
+{
+	@Value("${dbUsername}")
+	private String dbUsername;
 
-	/* (non-Javadoc)
-	 * @see com.pluralsight.repository.CustomerRepository#findAll()
+	/*
+	 * public void setDbUsername(String dbUsername) { this.dbUsername =
+	 * dbUsername; }
 	 */
 	@Override
-	public List<Customer> findAll() {
+	public List<Customer> findAll()
+	{
+		System.out.println(dbUsername);
 		List<Customer> customers = new ArrayList<>();
-		Customer customer  = new Customer();
+		Customer customer = new Customer();
 		customer.setFirstName("Brian");
 		customer.setLastName("Luckau");
 		customers.add(customer);
